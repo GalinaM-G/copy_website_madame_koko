46b027bee5ffb7ca2b42a0a87a7e78be7b741ff6
Galina <galina.mckoy@gmail.com>
2023-01-16 11:15:48 +0100

    added slider swiper neen to edit

--- js/main.js
+++ js/main.js
@@ -1,8 +1,8 @@
+// tabs
+
 const offerCont = document.querySelector(".offer__content");
 const btns = document.querySelectorAll(".offer__btn");
 const tabs = document.querySelectorAll(".offer__item");
-
-
 
 offerCont.addEventListener("click", function (e) {
     const id = e.target.dataset.id;
@@ -20,3 +20,33 @@ offerCont.addEventListener("click", function (e) {
         element.classList.add("active");
     }
 });
+
+
+const swiper = new Swiper('.swiper', {
+    
+    direction: 'horizontal',
+    loop: true,
+    spaceBetween: 10,
+    breakpoints: {
+        // when window width is >= 320px
+        320: {
+            slidesPerView: 1,
+            
+        },
+        // when window width is >= 480px
+        480: {
+            slidesPerView: 2,
+        },
+        // when window width is >= 640px
+        640: {
+            slidesPerView: 5,
+        }
+    },
+   
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+    },
+
+});
+
